681e86c2168c88214b0a2e9a87e7fdeb7d145aac
Steven Brooks Mogbo <stevenmogbo@yahoo.com>
2020-09-30 19:21:40 -0400

    update

--- config/connection.js
+++ config/connection.js
@@ -2,16 +2,16 @@
 // Set up MySQL connection.
 const mysql = require('mysql');
 
-if(process.JAWSDB_URL){
+if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-}else {connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "burger_db"
-});
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  });
 }
-
 connection.connect();
 
 // Export connection for our ORM to use.
